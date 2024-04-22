68f95697b6d2ee6143f0710bf01b913963cc100f
JadeMawn <jade.mawn@richmond.edu>
2024-04-22 01:54:17 -0400

    Updates

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function toggleDropdown() {
-    var dropdownMenu = document.getElementById("recipesDropdown");
+    var dropdownMenu = document.getElementById("Dropdown");
     if (dropdownMenu.style.display === "block") {
         dropdownMenu.style.display = "none";
     } else {
         dropdownMenu.style.display = "block";
+        dropdownMenu.onmouseleave = function() {
+            dropdownMenu.style.display = "none";
+        };
+        dropdownMenu.onmouseenter = function() {
+            dropdownMenu.style.display = "block";
+        };
     }
 }
