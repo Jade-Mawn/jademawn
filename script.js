0b36f11484b9719abcf59ecfe203f3bab78145b6
JadeMawn <jade.mawn@richmond.edu>
2024-09-29 20:37:26 -0400

    Updates

--- script.js
+++ script.js
@@ -12,22 +12,3 @@ function toggleDropdown() {
         };
     }
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-    var link = document.getElementById("glow-link");
-    var circle = document.getElementById("glow-circle");
-    
-    link.addEventListener("mousemove", function(event) {
-        var x = event.clientX - 50; // Adjust the offset of the circle
-        var y = event.clientY - 50; // Adjust the offset of the circle
-        circle.style.display = "block";
-        circle.style.left = x + "px";
-        circle.style.top = y + "px";
-    });
-    
-    link.addEventListener("mouseleave", function() {
-        circle.style.display = "none";
-    });
-});
-
-
